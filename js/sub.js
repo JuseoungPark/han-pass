468ff7e28c1c2557cd4295763a671bfc91505d8f
lee KR <benden4777@gmail.com>
2021-11-11 23:13:20 +0900

    navbar css 수정, 서브 js 보완

--- js/sub.js
+++ js/sub.js
@@ -8,23 +8,32 @@ $(document).ready(function(){
 })
 
 $(window).resize(function(){
-	var winWidth = $(window).width();
-	console.log(winWidth);
+	// resize시 class 리셋
 	$('#navbar-allmenu').removeClass('show');
 });
 
 $(window).scroll(function() {
-	console.log('scroll')
+	//console.log('scroll')
 	infoEvents()
 });
 
 function infoEvents(){
     let scrollTop = $(window).scrollTop();
-    if(scrollTop > $('.cont-wrap div:first').offset().top) {
-        $('#top-btn').stop().fadeIn();
+	var winWidth = $(window).width();
+	//console.log('infoEvents 발생, 넓이는 ' + winWidth);
+	if(scrollTop > $('.cont-wrap div:first').offset().top) {
+		// 스크롤을 내릴 때
+		$('#top-btn').stop().fadeIn();
 		$('.navbar').addClass('on');
-    } else {
-        $('#top-btn').stop().fadeOut();
-		$('.navbar').removeClass('on');
-    }
+	} else {
+		// 스크롤을 다 올렸을 때
+		$('#top-btn').stop().fadeOut();
+		// 1281보다 좁으면 (mobile)
+		if(winWidth < 1281){
+			$('.navbar').addClass('on');
+		// 1281보다 넓으면 (pc)
+		} else {
+			$('.navbar').removeClass('on');
+		}
+	}
 }
