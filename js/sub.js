69468f703eceed308d40686d167e5a0da6090452
lee KR <benden4777@gmail.com>
2021-11-11 23:39:44 +0900

    navbar 어색한 부분 수정

--- js/sub.js
+++ js/sub.js
@@ -10,17 +10,29 @@ $(document).ready(function(){
 $(window).resize(function(){
 	// resize시 class 리셋
 	$('#navbar-allmenu').removeClass('show');
+	navbarShow();
 });
 
 $(window).scroll(function() {
-	//console.log('scroll')
-	infoEvents()
+	infoEvents();
 });
+
+
+// resize시 navbar class mobile/pc 분기 이벤트
+function navbarShow() {
+	// 1281보다 좁으면 (mobile)
+	let winWidth = $(window).width();
+	if(winWidth < 1281){
+		$('.navbar').addClass('on');
+	}
+	// 1281보다 넓으면 (pc)
+	else {
+		$('.navbar').removeClass('on');
+	}
+}
 
 function infoEvents(){
     let scrollTop = $(window).scrollTop();
-	var winWidth = $(window).width();
-	//console.log('infoEvents 발생, 넓이는 ' + winWidth);
 	if(scrollTop > $('.cont-wrap div:first').offset().top) {
 		// 스크롤을 내릴 때
 		$('#top-btn').stop().fadeIn();
@@ -28,12 +40,6 @@ function infoEvents(){
 	} else {
 		// 스크롤을 다 올렸을 때
 		$('#top-btn').stop().fadeOut();
-		// 1281보다 좁으면 (mobile)
-		if(winWidth < 1281){
-			$('.navbar').addClass('on');
-		// 1281보다 넓으면 (pc)
-		} else {
-			$('.navbar').removeClass('on');
-		}
+		navbarShow();
 	}
 }
